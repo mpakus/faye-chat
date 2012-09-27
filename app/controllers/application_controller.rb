cd71b366c1b030d9ce9327abba32564c62dda476
angelamancini <angeladmancini@gmail.com>
2012-09-27 11:32:44 -0400

    added chatroom userlist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_url, :alert => "First log in to view this page."
   end
+  
+  helper_method :current_users_list
+  protected
+  def current_users_list
+    current_users.map {|u| u.username.titleize}.join("</li><li>").html_safe
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120925234905) do
+ActiveRecord::Schema.define(:version => 20120927150333) do
 
   create_table "users", :force => true do |t|
     t.string   "username",                        :null => false
@@ -25,8 +25,12 @@ ActiveRecord::Schema.define(:version => 20120925234905) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.datetime "last_login_at"
+    t.datetime "last_logout_at"
+    t.datetime "last_activity_at"
   end
 
+  add_index "users", ["last_logout_at", "last_activity_at"], :name => "index_users_on_last_logout_at_and_last_activity_at"
   add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
 
